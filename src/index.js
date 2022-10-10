130aca13164944cef58bdcbfb462663949dc24b6
Hugo Bally <admin@hugobally.me>
2022-10-10 22:24:29 +0200

    try debug cloudinary

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ const { syncWithDiscogs } = require('../utils')
 module.exports = {
   async bootstrap({ strapi }) {
     await syncWithDiscogs(strapi)
+    console.log(process.env.CLOUDINARY_KEY)
+    console.log(process.env.CLOUDINARY_NAME)
+    console.log(process.env.CLOUDINARY_SECRET)
   },
 }
